c33ff0c441f00a6af9d42da3a0d81a09933a3eb2
sorbetes6 <reuelgumawid6@gmail.com>
2024-02-23 16:22:46 +0800

    try gawa calendar

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,20 +1,20 @@
-<div class="container mx-auto my-8">
+<div>
     <div id="calendar"></div>
-
-    @push('scripts')
-        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.10.2/dist/fullcalendar.min.js"></script>
-        <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                var calendarEl = document.getElementById('calendar');
-
-                var calendar = new FullCalendar.Calendar(calendarEl, {
-                    plugins: ['dayGrid'],
-                    themeSystem: 'tailwind',
-                    height: 'auto',
-                });
-
-                calendar.render();
-            });
-        </script>
-    @endpush
 </div>
+
+@push('scripts')
+    <script>
+        document.addEventListener('livewire:load', function () {
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                plugins: ['dayGrid'],
+                events: [
+                    // Your events data here
+                ],
+            });
+
+            calendar.render();
+        });
+    </script>
+@endpush
--- resources/views/livewire/nav-bar.blade.php
+++ resources/views/livewire/nav-bar.blade.php
@@ -17,7 +17,6 @@
       
         </style>
     </head>
-
 <div>
 
         <!--Top Navigation-->
