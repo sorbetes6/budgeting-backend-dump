ca822acc2e9395e53680b3a7b02413ab3bbd5a44
alrlabajo <alrlabajo@gmail.com>
2024-02-28 15:28:21 +0800

    ppmp done

--- resources/views/livewire/p-p-m-p.blade.php
+++ resources/views/livewire/p-p-m-p.blade.php
@@ -24,6 +24,7 @@
         </div>
     </div>
 
+    <!--Row 1-->
     <div class="flex my-5">
         <!--Card 3-->
         <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex">
@@ -44,7 +45,7 @@
             </form>
         </div>
 
-        <!--Card 4-->
+        <!--Card 5-->
         <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Item/Name</label>
@@ -69,5 +70,73 @@
                 </div>
             </form>
         </div>
+    </div>
+
+    <!--Row 2-->
+    <div class="flex my-5">
+        <!--Card 3-->
+        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex">
+            <form class="space-y-2" action="#">
+                <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Quantity</label>
+                <div class="w-40 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex">
+                    <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="0">
+                </div>
+            </form>
+        </div>
+        <!--Card 4-->
+        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+            <form class="space-y-2" action="#">
+                <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Account Code</label>
+                <div class="w-40 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex">
+                    <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                </div>
+            </form>
+        </div>
+
+        <!--Card 5-->
+        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+            <form class="space-y-2" action="#">
+                <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Description</label>
+                <div class="w-40 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex">
+                    <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                </div>
+            </form>
+        </div>
+        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+            <form class="space-y-2" action="#">
+                <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Procurement Method</label>
+                <div class="w-40 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex">
+                    <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                </div>
+            </form>
+        </div>
+        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+            <form class="space-y-2" action="#">
+                <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Estimated Budget</label>
+                <div class="w-40 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex">
+                    <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱0.00">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="flex flex-col">
+        <!--Schedule-->
+        <div class="w-[1191px] h-40 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex">
+            <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Delivery/Implementation Schedule</label>
+        </div>
+
+        <button class="w-44 h-9 px-4 py-2 bg-white rounded-md shadow border border-zinc-200 justify-center items-center gap-2 inline-flex my-6">
+            <div class="w-4 h-4 relative">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                    <path d="M3.33301 8.00065H12.6663M7.99967 3.33398V12.6673" stroke="#18181B" stroke-width="1.33" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+            <label type="button" class="text-zinc-950 text-sm font-medium font-['Inter'] leading-tight">Add another data</label>
+        </button>
+    </div>
+
+    <div class="flex flex-row-reverse">
+        <button class="w-32 h-12 px-4 py-2 bg-indigo-800 rounded-md shadow justify-center items-center gap-2 inline-flex text-white text-base font-medium font-['Inter'] leading-tight">Submit</button>
     </div>
 </div>
