7ae3639b755e118063d0742f7fcfef94affd0e8a
alrlabajo <alrlabajo@gmail.com>
2024-03-02 09:31:23 +0800

    added another row capital outlay

--- resources/views/livewire/capital-outlay.blade.php
+++ resources/views/livewire/capital-outlay.blade.php
@@ -112,6 +112,60 @@
                             </div>
                         </td>
                     </tr>
+                    <tr class="gap-6">
+                        <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            1-07-05-030
+                        </th>
+                        <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            Information and Communication Technology Equipment
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
+                            </div>
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="gap-6">
+                        <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            1-07-05-070
+                        </th>
+                        <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            Communication Equipment
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
+                            </div>
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="gap-6">
+                        <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            1-07-05-090
+                        </th>
+                        <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            Disaster Response and Rescue Equipment
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
+                            </div>
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                            </div>
+                        </td>
+                    </tr>
         
                     
                 </tbody>
