447903e4e8cb3cd466fa596ed7e0cf19f7638c9a
alrlabajo <alrlabajo@gmail.com>
2024-03-03 21:59:36 +0800

    capital outlay done + added functionalities navbar

--- resources/views/livewire/capital-outlay.blade.php
+++ resources/views/livewire/capital-outlay.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="w-[1218px] h-[550px] bg-white rounded-xl shadow border border-zinc-200">
+    <div class="w-[1218px] h-[640px] bg-white rounded-xl shadow border border-zinc-200">
 
         <div>
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -112,10 +112,10 @@
                     </tr>
                     <tr class="gap-6">
                         <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
-                            1-07-05-030
+                            1-07-05-110
                         </th>
                         <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
-                            Information and Communication Technology Equipment
+                            Medical Equipment
                         </td>
                         <td class="px-6 py-2">
                             <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
@@ -130,10 +130,10 @@
                     </tr>
                     <tr class="gap-6">
                         <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
-                            1-07-05-070
+                            1-07-05-130
                         </th>
                         <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
-                            Communication Equipment
+                            Sports Equipment
                         </td>
                         <td class="px-6 py-2">
                             <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
@@ -148,10 +148,82 @@
                     </tr>
                     <tr class="gap-6">
                         <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
-                            1-07-05-090
+                            1-07-05-140
                         </th>
                         <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
-                            Disaster Response and Rescue Equipment
+                            Technical and Scientific Equipment
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
+                            </div>
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="gap-6">
+                        <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            1-07-05-990
+                        </th>
+                        <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            Other Machinery and Equipment
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
+                            </div>
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="gap-6">
+                        <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            1-07-06-010
+                        </th>
+                        <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            Transportation Equipment
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
+                            </div>
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="gap-6">
+                        <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            1-07-07-010
+                        </th>
+                        <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            Furniture and Fixtures
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
+                            </div>
+                        </td>
+                        <td class="px-6 py-2">
+                            <div class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200items-center gap-2 inline-flex">
+                                <input type="justification" name="justification" id="justification" class="w-30 bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="gap-6">
+                        <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            1-07-07-020
+                        </th>
+                        <td class="pr-10 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                            Books
                         </td>
                         <td class="px-6 py-2">
                             <div class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex">
