1ce0c20c0564f67696a579fb9f9c96176df44726
sorbetes6 <reuelgumawid6@gmail.com>
2024-03-15 19:15:53 +0800

    fixed ppmp form

--- resources/views/livewire/ppmp-form.blade.php
+++ resources/views/livewire/ppmp-form.blade.php
@@ -1,23 +1,23 @@
 <div>
-    <div class="flex">
+    <div class="flex w-[1400px]">
         <!--Card 1-->
-        <div class="w-[583px] h-[143px] px-8 py-6 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
-            <form class="space-y-4" action="#">
-                <div class="h-6 flex-col justify-start items-start gap-3 flex">
+        <div class="w-[585px] h-[90px] px-5 py-6 bg-white border border-gray-200 rounded-lg shadow sm:p-4">
+            <form class="space-y-2" action="#">
+                <div class="h-6 flex-col justify-start items-start flex">
                     <label class="text-zinc-950 text-base font-semibold font-['Inter'] leading-normal">Program Title</label>
                     <div class="flex-grow-0 flex-shrink-0">
-                        <input type="cost-student" name="cost-student" id="cost-student" class="w-fit h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                        <input type="cost-student" name="cost-student" id="cost-student" class="w-[550px] h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                     </div>
                 </div>
             </form>
         </div>
         <!--Card 2-->
-        <div class="w-[583px] h-[143px] px-8 py-6 bg-white border border-gray-200 rounded-lg shadow sm:p-8 mx-4">
-            <form class="space-y-4" action="#">
-                <div class="h-6 flex-col justify-start items-start gap-3 flex">
+        <div class="w-[585px] h-[90px] px-5 py-6 bg-white border border-gray-200 rounded-lg shadow sm:p-4 mx-[30px]">
+            <form class="space-y-2" action="#">
+                <div class="h-6 flex-col justify-start items-start flex">
                     <label class="text-zinc-950 text-base font-semibold font-['Inter'] leading-normal">Program Title</label>
                     <div class="flex-grow-0 flex-shrink-0">
-                        <input type="cost-student" name="cost-student" id="cost-student" class="w-fit h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                        <input type="cost-student" name="cost-student" id="cost-student" class="w-[550px] h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                     </div>
                 </div>
             </form>
@@ -25,104 +25,104 @@
     </div>
 
     <!--Row 1-->
-    <div class="flex my-5">
+    <div class="flex my-5 w-[1400px]">
         <!--Card 3-->
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex">
+        <div class="w-[220px] h-24 px-4 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start inline-flex">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Type of Contract</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                 </div>
             </form>
         </div>
         <!--Card 4-->
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+        <div class="w-[220px] h-24 px-4 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-[25px]">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Account Title</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                 </div>
             </form>
         </div>
 
         <!--Card 5-->
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+        <div class="w-[220px] h-24 px-4 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-[25px]">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Item/Name</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                 </div>
             </form>
         </div>
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+        <div class="w-[220px] h-24 px-4 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-[25px]">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Unit of Issue</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                 </div>
             </form>
         </div>
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+        <div class="w-[220px] h-24 px-4 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-[25px]">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Unit Price</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱0.00" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱0.00" />
                 </div>
             </form>
         </div>
     </div>
 
     <!--Row 2-->
-    <div class="flex my-5">
+    <div class="flex my-5 w-[1400px]">
         <!--Card 3-->
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex">
+        <div class="w-[220px] h-24 px-4 py-2 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Quantity</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="0" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="0" />
                 </div>
             </form>
         </div>
         <!--Card 4-->
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+        <div class="w-[220px] h-24 px-4 py-2 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-[25px]">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Account Code</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                 </div>
             </form>
         </div>
 
         <!--Card 5-->
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+        <div class="w-[220px] h-24 px-4 py-2 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-[25px]">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Description</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                 </div>
             </form>
         </div>
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+        <div class="w-[220px] h-24 px-4 py-2 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-[25px]">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Procurement Method</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description" />
                 </div>
             </form>
         </div>
-        <div class="w-52 h-24 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-9">
+        <div class="w-[220px] h-24 px-4 py-2 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex ml-[25px]">
             <form class="space-y-2" action="#">
                 <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Estimated Budget</label>
                 <div class="flex-grow-0 flex-shrink-0">
-                    <input type="cost-student" name="cost-student" id="cost-student" class="w-32 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱0.00" />
+                    <input type="cost-student" name="cost-student" id="cost-student" class="w-40 h-8 px-3 py-2 bg-white rounded-md shadow border border-zinc-200 justify-start items-center gap-2 inline-flex focus:ring-blue-500 focus:border-blue-500 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱0.00" />
                 </div>
             </form>
         </div>
     </div>
 
+    <!--Schedule-->
     <div class="flex flex-col">
-        <!--Schedule-->
-        <div class="w-[1191px] h-40 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex">
+        <div class="w-[1200px] h-40 px-5 py-3.5 bg-white rounded-xl shadow border border-zinc-200 flex-col justify-start items-start gap-2.5 inline-flex">
             <label class="text-zinc-950 text-sm font-medium font-['Inter'] leading-normal">Delivery/Implementation Schedule</label>
         </div>
 
@@ -136,7 +136,7 @@
         </button>
     </div>
 
-    <div class="flex flex-row-reverse">
-        <button class="w-30 h-10 px-4 py-2 bg-indigo-800 rounded-md shadow justify-center items-center gap-2 inline-flex text-white text-base font-medium font-['Inter'] leading-tight">Submit</button>
+    <div class="w-[1200px] flex justify-end">
+        <button class="w-30 h-10 px-4 py-2 bg-indigo-800 rounded-md shadow justify-center items-center text-white text-base font-medium font-['Inter'] leading-tight">Submit</button>
     </div>
 </div>
