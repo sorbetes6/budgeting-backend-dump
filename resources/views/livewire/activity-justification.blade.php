20c10fb6f8a472769e241b85dc50e1138cd23544
sorbetes6 <reuelgumawid6@gmail.com>
2024-03-16 16:53:25 +0800

    revised top bar budget call records

--- resources/views/livewire/activity-justification.blade.php
+++ resources/views/livewire/activity-justification.blade.php
@@ -1,14 +1,19 @@
-<div class="flex w-[1420px] mx-[270px] my-[110px]">
-    <div class="w-[1420px] mx-[0px] rounded-lg p-4 relative">
-        <div class="absolute shadow inset-x-0 bottom-0 h-[1px] bg-gray-200"></div>
-        <div class="flex flex-col gap-y-[5px] text-indigo-800 text-[25px] font-extrabold font-['Inter'] leading-5">Activity Justification
-            <div class="flex flex-row items-center">
-                <label class="text-yellow-700 text-sm font-normal font-['Inter'] leading-loose mr-1">Budget Call Forms ></label>
-                <label class="text-black text-sm font-normal font-['Inter'] leading-loose">Activity Justification Form No.</label>
-            </div>
-            <div class="flex ml-[835px] mt-[-50px] items-start">
-                @include('livewire.import-export-add-buttons')
+<div>   
+    
+    <!-- Top Bar -->
+    <div class="flex w-[1420px] mx-[270px] my-[110px]">
+        <div class="w-[1420px] mx-[0px] rounded-lg p-4 relative">
+            <div class="absolute shadow inset-x-0 bottom-0 h-[1px] bg-gray-200"></div>
+            <div class="flex flex-col gap-y-[5px] text-indigo-800 text-[25px] font-extrabold font-['Inter'] leading-5">Activity Justification
+                <div class="flex flex-row items-center">
+                    <label class="text-yellow-700 text-sm font-normal font-['Inter'] leading-loose mr-1">Budget Call Forms ></label>
+                    <label class="text-black text-sm font-normal font-['Inter'] leading-loose">Activity Justification Form No.</label>
+                </div>
+                <div class="flex ml-[835px] mt-[-50px] items-start">
+                    @include('livewire.import-export-add-buttons')
+                </div>
             </div>
         </div>
     </div>
+
 </div>
--- resources/views/livewire/mooe.blade.php
+++ resources/views/livewire/mooe.blade.php
@@ -1,4 +1,7 @@
-<div class="flex w-[1420px] mx-[270px] my-[110px]">
+<div>
+    
+    <!-- Top Bar -->    
+    <div class="flex w-[1420px] mx-[270px] my-[110px]">
     <div class="w-[1420px] mx-[0px] rounded-lg p-4 relative">
         <div class="absolute shadow inset-x-0 bottom-0 h-[1px] bg-gray-200"></div>
         <div class="flex flex-col gap-y-[5px] text-indigo-800 text-[25px] font-extrabold font-['Inter'] leading-5">Maintenance and Other Operating Expenses
@@ -11,4 +14,6 @@
             </div>
         </div>
     </div>
+    </div>
+
 </div>
