80e49e82570a80fa2824785457c2bb1cd11ea41b
alrlabajo <alrlabajo@gmail.com>
2024-04-12 00:10:19 +0800

    fixed modals but not yet displayed

--- resources/views/livewire/edit-modal.blade.php
+++ resources/views/livewire/edit-modal.blade.php
@@ -16,7 +16,7 @@
             <button data-modal-target="edit-item-modal" data-modal-toggle="edit-item-modal" class="px-4 py-2" type="button">Edit</button>
             <!-- Edit modal -->
             <div id="edit-item-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div class="bg-white rounded-lg shadow-lg w-full max-w-md mx-6 md:mx-0 p-4 md:p-6">
+                <div class="relative p-4 w-full max-w-2xl max-h-full">
                     <!-- Modal content -->
                     <div class="relative">
                         <!-- Modal header -->
