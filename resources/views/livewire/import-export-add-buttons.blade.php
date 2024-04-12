a696ef6d2d19857532ba6c4c086a28c942e8c280
alrlabajo <alrlabajo@gmail.com>
2024-04-12 10:31:15 +0800

    removed sort by

--- resources/views/livewire/import-export-add-buttons.blade.php
+++ resources/views/livewire/import-export-add-buttons.blade.php
@@ -28,28 +28,6 @@
         </ul>
     </div>
 
-    <!--Sort -->
-    <button id="SortDropdown" data-dropdown-toggle="sortDropdown" class="w-[150px] h-10 pl-2 py-1 bg-white rounded-lg border border-zinc-300 justify-start items-center gap-2 inline-flex">
-        <div class="w-5 h-6 relative">
-            <svg class="w-2.5 h-2.5 m-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-            </svg>
-        </div>
-        <span class="text-black text-sm font-normal font-inter leading-tight text-center">Sort records by</span>
-    </button>
-
-
-    <!-- Sort Dropdown menu -->
-    <div id="sortDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="SortDropdown">
-            <li>
-                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Newest First</a>
-            </li>
-            <li>
-                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Oldest First</a>
-            </li>
-        </ul>
-    </div>
 
     <!-- Import Modal toggle -->
     <button data-modal-target="import-modal" data-modal-toggle="import-modal" class="w-20 h-10 px-4 bg-indigo-50 rounded-lg shadow border border-indigo-50 justify-center items-center gap-2 inline-flex text-indigo-800 text-sm font-semibold font-['Inter'] leading-tight" type="button">Import</button>
