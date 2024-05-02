ec1e4b7859d7cb64f3d7d4dec57bcbc07353a008
yatootz23 <amiellabajo05@gmail.com>
2024-05-02 10:18:03 +0800

    fixed layout

--- resources/views/livewire/amendment.blade.php
+++ resources/views/livewire/amendment.blade.php
@@ -1,9 +1,9 @@
-<div>
+<div class="flex flex-col grid grid-cols-1 md:grid-cols-1 sm:grid-cols-1 px-7 py-32">
     <!-- Top Bar -->
-   <div class="flex w-[1420px] mx-[270px] mt-[110px] mb-[10px]">
-        <div class="w-[1420px] mx-[0px] rounded-lg p-4 relative">
+   <div class="flex w-[1420px]">
+        <div class="w-[1420px] rounded-lg p-4 relative">
             <div class="absolute shadow inset-x-0 bottom-0 h-[1px] bg-gray-200"></div>
-            <div class="flex flex-col gap-y-[5px] text-indigo-800 text-[25px] font-extrabold font-['Inter'] leading-5">Amendment
+            <div class="flex flex-col gap-y-1.5 text-indigo-800 text-[25px] font-extrabold font-['Inter'] leading-5">Amendment
                 <div class="flex flex-row items-center">
                     <label class="text-yellow-700 text-sm font-normal font-['Inter'] leading-loose mr-1">Payment Request ></label>
                     <label class="text-black text-sm font-normal font-['Inter'] leading-loose">Amendment Form No.</label>
